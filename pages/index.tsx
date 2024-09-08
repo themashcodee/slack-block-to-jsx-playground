ef5833e57760d3300794461435a4d1a33bcdb7f8
themashcodee <codeemash@gmail.com>
2024-09-08 05:32:43 +0530

    chore: ✨ add temlist link

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,21 @@ export default function Home() {
 		<main
 			className={`overflow-auto min-h-screen bg-[#F6F4F1] ${font.className}`}
 		>
+			<div className="w-full py-1 px-2 bg-gradient-to-r from-red-500 to-orange-500 text-white">
+				<p className="text-center text-sm">
+					Craft perfect Slack and Teams messages in seconds with ready-made
+					templates!{" "}
+					<a
+						href="https://temlist.vercel.app/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="underline"
+					>
+						Visit Temlist
+					</a>
+				</p>
+			</div>
+
 			<Editor />
 		</main>
 	)
