b6516d71509b9a5f16c5383e6a0b618cae41b92f
Mash Codee <codeemash@gmail.com>
2026-03-20 13:47:57 +0530

    chore: 🔧 switch to yarn n update library version to v1

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "a.slack-edge.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "github.githubassets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn-icons-png.flaticon.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "**", // Allow any HTTPS domain for user-provided logos
-      },
-    ],
-  },
-};
+	reactStrictMode: true,
+	experimental: {
+		esmExternals: "loose",
+	},
+	transpilePackages: ["slack-blocks-to-jsx", "react-markdown", "remark-gfm"],
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "a.slack-edge.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "github.githubassets.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "cdn-icons-png.flaticon.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "**", // Allow any HTTPS domain for user-provided logos
+			},
+		],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
